e2f094f0b9f63d347695390373cdefbff969d8a1
cadorn <christoph@christophdorn.com>
2012-08-22 15:53:09 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Sourcemint Node
+===============
+
+*Status: ALPHA*
+
+The sourcemint networkable node daemon to be installed on all systems.
+
+  * Copyright: 2012 [Christoph Dorn](http://www.christophdorn.com/)
+  * Code License: [MIT License](http://www.opensource.org/licenses/mit-license.php)
+  * Docs License: [Creative Commons Attribution-NonCommercial-ShareAlike 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/)
+  * Sponsor: [Sourcemint](http://sourcemint.com/)
+  * Mailing list: [groups.google.com/group/sourcemint](http://groups.google.com/group/sourcemint)
---
+++ server.js
@@ -0,0 +1,32 @@
+
+const EXPRESS = require("express");
+const SM_NODE_CLIENT = require("sourcemint-node/lib/client");
+
+
+var app = EXPRESS();
+
+app.use(EXPRESS.bodyParser());
+
+app.post("/api/broadcast", function(req, res) {
+
+    if (req.body && req.body.payload) {
+        var payload = false;
+        try {
+            payload = JSON.parse(req.body.payload);
+        } catch(err) {
+            console.error(err.stack);
+            res.writeHead(500);
+            res.end("Error parsing payload!");
+            return;
+        }
+
+console.log("payload", payload);
+
+        res.send("OK");
+        return;
+    }
+    res.writeHead(400);
+    res.end();
+});
+
+app.listen(9998);
